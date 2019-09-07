c96ef2e85512673bbff3d67710930bd009953c56
Arthur <kingart808@gmail.com>
2019-09-07 11:29:56 -1000

    finally done but im sure it can be done better

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function locate(array, target) {
-  // do work here
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return true;
+    }
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === target) {
+        return true;
+      }
+      for (let x = 0; x < array[i][j].length; x++) {
+        if (array[i][j][x] === target) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 module.exports = locate;
